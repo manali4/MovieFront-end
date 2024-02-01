ffe1b7f6a1fd86acd15dee05afb2a452ca4d2d2b
manali4 <manalimodi48@gmail.com>
2024-01-31 16:02:08 -0800

    frontend-code

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import React from "react";
+
+const Layout = () => {
+    return(
+        <main>
+            <Outlet>
+                
+            </Outlet>
+
+        </main>
+    )
+}
+
+export default Layout
